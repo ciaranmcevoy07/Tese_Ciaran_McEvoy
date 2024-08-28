317d88a3ab3ce31244413839570f7e18b1eff681
Ciaran McEvoy <ciaranmcevoy07@gmail.com>
2024-08-28 01:56:39 +0100

    dsds

--- Statistics/PCA.r
+++ Statistics/PCA.r
@@ -12,6 +12,7 @@ filtered_data <- dataset %>%
   filter(!(Group == "MCI" & Age > 70))
 
 table <- filtered_data[, 1046:1080]
+# table <- dataset[, 1046:1080]
 
 ############ PCA Analysis for AD Subjects #####################
 
@@ -42,27 +43,27 @@ fviz_pca_var(data.pca, col.var="contrib")+
 # print(fviz_pca_var(data.pca, col.var = "cos2",
 #             gradient.cols = c("black", "orange", "green"),
 #             repel = TRUE))
-# print(fviz_pca_ind(data.pca, label="none", habillage=dataset$Group,
-#         addEllipses=TRUE, ellipse.level=0.30, select.ind = list(cos2 = 100)) + labs(title ="PCA", x = "PC1", y = "PC2"))
+print(fviz_pca_ind(data.pca, label="none", habillage=filtered_data$Group,
+        addEllipses=TRUE, ellipse.level=0.30, select.ind = list(cos2 =100)) + labs(title ="PCA", x = "PC1", y = "PC2"))
 
 # Create the PCA biplot and save to a file
-p <- fviz_pca_biplot(data.pca, 
-                # Individuals
-                geom.ind = "point",
-                fill.ind = filtered_data$Group, col.ind = "black",
-                pointshape = 21, pointsize = 2,
-                palette = "jco",
-                addEllipses = TRUE,
-                ellipse.level=0.30,
-                select.ind = list(cos2 = 100),
-                select.var = list(contrib = 5),
-                # Variables
-                alpha.var ="contrib", col.var = "contrib",
-                gradient.cols = "RdYlBu",
+# p <- fviz_pca_biplot(data.pca, 
+#                 # Individuals
+#                 geom.ind = "point",
+#                 fill.ind = filtered_data$Group, col.ind = "black",
+#                 pointshape = 21, pointsize = 2,
+#                 palette = "jco",
+#                 addEllipses = TRUE,
+#                 ellipse.level=0.30,
+#                 select.ind = list(cos2 = 100),
+#                 select.var = list(contrib = 5),
+#                 # Variables
+#                 alpha.var ="contrib", col.var = "contrib",
+#                 gradient.cols = "RdYlBu",
                 
-                legend.title = list(fill = "Groups", color = "Contrib",
-                                    alpha = "Contrib")
-                )
+#                 legend.title = list(fill = "Groups", color = "Contrib",
+#                                     alpha = "Contrib")
+#                 )
 
 # Save the plot
-ggsave("PCA.pdf", plot = p, width = 10, height = 15, limitsize = FALSE)
+# ggsave("PCA.pdf", plot = p, width = 10, height = 15, limitsize = FALSE)
